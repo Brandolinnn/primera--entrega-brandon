96376875e0183daa3aa7bd6d415806015c5dde21
brandolin <brandonacosta980@gmail.com>
2022-07-18 16:52:15 -0300

    segundo commit

--- js/main.js
+++ js/main.js
@@ -42,16 +42,30 @@ if (productosCategorias) {
     alert("DEBE INGRESAR UNA CATEGORIA QUE EXISTA");
 } 
 
+const carrito =[];
+
+
+
 let comprar = prompt ("DESEA COMPRAR ALGUN PRODUCTO ? \n RESPONDER SI PARA SEGUIR O NO PARA SALIR").toUpperCase();
+let productoSeleccionado;
 
-if (comprar == "SI") {
-    let productoSeleccionado = prompt ("QUE PRODUCTO DESEA COMPRAR ?").toUpperCase();
-    alert("EL PRODUCTO "+productoSeleccionado+" FUE AGREGADO AL CARRITO");
-
-} else {
-    alert("GRACIAS POR SU ATENCION")
-
+function funcionCompra() {
+    if (comprar == "SI") {
+        productoSeleccionado = prompt ("QUE PRODUCTO DESEA COMPRAR ?").toUpperCase();
+        alert("SU PRODUCTO FUE AGREGADO CORRECTAMENTE ")
+        carrito.push(productoSeleccionado);
+        alert ("ESTOS PRODUCTOS AGREGO A SU CARRITO: " +carrito)
+    } else {
+        alert("GRACIAS POR SU ATENCION")
+    }
+    
 }
+
+funcionCompra();
+
+comprar = prompt("QUIERE SEGUIR COMPRANDO ? \n RESPONDER SI PARA SEGUIR O NO PARA SALIR").toUpperCase();
+
+funcionCompra();
 
 
 
